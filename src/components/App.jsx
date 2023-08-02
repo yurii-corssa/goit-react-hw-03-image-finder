9318928e6093f486159f4093cdee91a373b9097b
yurii-corssa <yurii.corssa@gmail.com>
2023-08-02 19:25:08 +0200

    project preparation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
 import { GlobalStyle } from './App.styled';
-import { ContactsBook } from './ContactsBook/ContactsBook';
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
-      <ContactsBook />
     </>
   );
 };
--- src/components/ContactsBook/Notification/Notification.jsx
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types';
-import { Notice } from './Notification.styled';
-
-export const Notification = ({ text }) => <Notice>{text}</Notice>;
-
-Notification.propTypes = { text: PropTypes.string };
