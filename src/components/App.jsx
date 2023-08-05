09f0fdf7420598ea228f478acf0033b0642832aa
yurii-corssa <yurii.corssa@gmail.com>
2023-08-05 13:52:13 +0200

    other settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,28 @@
-import { GlobalStyle } from './App.styled';
+import { Component } from 'react';
+import { GlobalStyle, Header, Section } from './App.styled';
+import { Searchbar } from './Searchbar/Searchbar';
+import { getPhotos } from './services/api';
 
-export const App = () => {
-  return (
-    <>
-      <GlobalStyle />
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    searchQuery: '',
+  };
+
+  handleSearchQuery = searchQuery => {
+    this.setState({ searchQuery });
+  };
+
+  render() {
+    getPhotos();
+    return (
+      <>
+        <GlobalStyle />
+        <Section>
+          <Header>
+            <Searchbar onSubmit={this.handleSearchQuery} />
+          </Header>
+        </Section>
+      </>
+    );
+  }
+}
